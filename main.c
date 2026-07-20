df241c962096c1c5489f64c64f35ce1b0986639f
ASRAFUL HASAN MINHAZ <ashrafulminhaj4@gmail.com>
2026-07-21 01:17:37 +0600

    Add C if-else program

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int apple =37,ios=37, android=14;
+
+     if (apple==ios)
+
+     {
+         printf("apple and ios are equal\n");
+     }
+     else if (apple==android)
+     {
+         printf ("apple and android is equal\n");
+     }
+
+    return 0;
+}
